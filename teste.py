59d8fe6a48e29cef730cb4dac8e6a30ec7d6f11e
fernanda332 <118773699+fernanda332@users.noreply.github.com>
2023-09-28 20:38:55 +0000

    questao10

---
+++ teste.py
@@ -0,0 +1,7 @@
+for x in range(1,16):
+    nome = input('Digite o nome do aluno: ')
+    nota1 = int(input('Digite a nota da prova 1: '))
+    nota2 = int(input('Digite a nota da prova 2: '))
+    media = (nota1 + nota2)
+    if media==nota1+nota2:
+print(media)
